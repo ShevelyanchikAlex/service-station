7d0c77057dcb7d0730873d387ccc7747cf83419f
Nikita Fedarenchyk <nikita@MacBook-Pro-Nikita.local>
2022-03-21 22:27:58 +0300

    feat: add ability to get info from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 export default () => {
 
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get("http://localhost:5000/cars");
+            console.log(data);
+        }
+        search();
+
+    }, []);
 
     return (
         <div>
